b7c293ee9cb3b7696cab1c0219b5cff39a10fdc4
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2021-03-30 16:47:42 +1000

    example

---
+++ _targets.R
@@ -0,0 +1,14 @@
+## Load your packages, e.g. library(targets).
+source("./packages.R")
+
+## Load your R files
+lapply(list.files("./R", full.names = TRUE), source)
+
+plan(multisession)
+
+## tar_plan supports drake-style targets and also tar_target()
+tar_plan(
+
+  low_mpg = filter(mtcars < 20)
+
+)
---
+++ packages.R
@@ -0,0 +1,8 @@
+## library() calls go here
+library(conflicted)
+library(dotenv)
+library(targets)
+library(tarchetypes)
+library(tidyverse)
+library(future)
+conflicted::conflict_prefer("filter", "dplyr")
